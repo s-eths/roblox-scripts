d9dd31ce357042a7f65f91a1d6c8a77e9102c46e
seth <107208142+s-eths@users.noreply.github.com>
2025-02-11 01:37:13 +1100

    Create auto-sell-guns.lua

---
+++ DaHood/auto-sell-guns.lua
@@ -0,0 +1,15 @@
+getgenv().ENABLED = true;
+
+while ENABLED do
+    task.wait();
+    for i, v in next, game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui.WeaponSkinsGUI.Body.Wrapper.SkinView.Frame.Skins.Contents.Entries:GetChildren() do
+        if v.Name ~= "UIGridLayout" and v.Name ~= "UIPadding" and v.Name ~= "SkinEntryTemplate" then
+            if v.Preview.AmountValue.Text ~= "x0" then
+                local Amount = v.Preview.AmountValue.Text:gsub("x", "");
+                local Type, Name = v.Name:match("^(.-%])(.+)$");
+                game:GetService("ReplicatedStorage"):WaitForChild("MainEvent"):FireServer("SellSkins", Type, Name, Amount);
+                task.wait(1);
+            end;
+        end;
+    end;
+end;
